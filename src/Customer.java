f04eff4bda0aa884c6e89634840ee7576335b5a9
soobinua <dev7er@gmail.com>
2024-05-11 01:36:07 +0900

    텍스트를 HTML 형식으로 변환해 테스트

--- src/Customer.java
+++ src/Customer.java
@@ -66,4 +66,54 @@ public class Customer {
         return result;
     }
 
+    //텍스트를 HTML 형식으로 하기 위해 기존 메서드 복붙
+    public String htmlStatement() {
+
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+        String result = getName() + " <h1>고객님의 대여 기록</h1>";
+
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentals.nextElement();
+
+            // 비디오 종류별 대여료 계산
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            // 적립 포인트를 1 포인트 증가
+            frequentRenterPoints++;
+
+            // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    each.getDaysRented() > 1) frequentRenterPoints++;
+
+            // 이번에 대여하는 비디오 정보와 대여료를 출력
+            result += "<span>" + each.getMovie().getTitle() + "</span>" +
+                    "<span>" + String.valueOf(thisAmount) + "</span>";
+
+            // 현재까지 누적된 총 대여료
+            totalAmount += thisAmount;
+        }
+
+        // 푸터 행 추가
+        result += "<h2>누적 대여료 : " + String.valueOf(totalAmount) + "</h2>";
+        result += "<h2>적립 포인트 : " + String.valueOf(frequentRenterPoints) + "</h2>";
+        return result;
+    }
+
 }
--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -11,10 +11,32 @@ class CustomerTest {
         Customer customer = new Customer("soobinua");
 
         customer.addRental(rental);
-        String result = customer.statement();
+//        String result = customer.statement();
+        String result = "soobinua 고객님의 대여 기록\n" +
+                "\tfirst Movie\t9.0\n" +
+                "누적 대여료 : 9.0\n" +
+                "적립 포인트 : 2";
 
 //        System.out.println(result);
 
         Assertions.assertEquals(customer.statement(), result);
     }
+
+    @Test
+    void htmlStatement() {
+
+        Movie movie = new Movie("first Movie", Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie, 3);
+        Customer customer = new Customer("soobinua");
+
+        customer.addRental(rental);
+        //result 문자열로 .html 만들면 기존 텍스트에서 HTML 형식으로 바뀐 것 크롬에서 확인 가능
+        String result = "soobinua <h1>고객님의 대여 기록</h1><span>first Movie</span><span>9.0</span><h2>누적 대여료 : 9.0</h2><h2>적립 포인트 : 2</h2>";
+
+//        System.out.println(result);
+
+        Assertions.assertEquals(customer.htmlStatement(), result);
+    }
+
+
 }
