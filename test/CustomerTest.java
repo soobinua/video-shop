9f169ca60a5a113c543981cf941e1ff1e2573e9e
soobinua <dev7er@gmail.com>
2024-05-11 01:24:25 +0900

    비디오샵 분석 및 첫 실행

--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -5,5 +5,15 @@ class CustomerTest {
     @org.junit.jupiter.api.Test
     void statement() {
 
+        Movie movie = new Movie("first Movie", Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie, 3);
+        Customer customer = new Customer("soobinua");
+
+        customer.addRental(rental);
+        String result = customer.statement();
+
+        System.out.println(result);
+
+
     }
 }
